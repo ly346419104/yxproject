597f25a6da4e072359a7b8728d022732ad8648aa
ly <8574364+ly346419104@user.noreply.gitee.com>
2022-10-22 00:26:48 +0800

    redis集成

---
+++ src/main/java/com/yxproj/yxproject/configuration/RedisConfig.java
@@ -0,0 +1,37 @@
+//package com.yxproj.yxproject.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//
+////@Configuration
+//public class RedisConfig {
+//
+//
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//        return new JedisConnectionFactory();
+//    }
+//
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory);
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new StringRedisSerializer());
+//        template.setHashKeySerializer(new StringRedisSerializer());
+//        template.setHashValueSerializer(new StringRedisSerializer());
+//        return template;
+//    }
+//    @Bean
+//    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        StringRedisTemplate template = new StringRedisTemplate();
+//        template.setConnectionFactory(redisConnectionFactory);
+//        return template;
+//    }
+//}
